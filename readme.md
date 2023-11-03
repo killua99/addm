714300dc608328bb19628420a55bc8fb06d9e0d9
Luigi Guevara <luigi.fox15@gmail.com>
2023-11-04 00:23:17 +0100

    Prueba concepto creación de plantillas web

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Asombrosos Detectives de Monstruos
+
+Simple visor de plantillas para imprimir y usar en tu próxima aventura.
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Aqui encontraras las plantillas y herramientras que te ayudarán a introducir tus detectives.</template>
+  </WelcomeItem>
+
+</template>
